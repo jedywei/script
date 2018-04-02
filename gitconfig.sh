da85029e54031d6bbb8175adfd437d1a10b4e532
“jedy.wei” <jedy.wei@advantech.com.tw>
2018-04-02 18:16:56 +0800

    set git url default

---
+++ gitconfig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# git url config default
+
+git config remote.origin.url https://jedywei:71susislove@github.com/jedywei/vim.git
+git config remote.origin.url https://jedywei:71susislove@github.com/jedywei/script.git
+
