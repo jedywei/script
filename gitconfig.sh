38059b01aa64354a48529b3f34abf19cdd4779f8
“jedy.wei” <jedy.wei@advantech.com.tw>
2018-04-12 10:23:18 +0800

    add gitconfig.sh

---
+++ gitconfig.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# setting 
+git config --global user.email=jedy.wei@advantech.com.tw
+git config --global user.name=jedy.wei
+
+# alias
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -l HEAD'
