698f1301cc2983ed3d9beaa01c87cd72e9e5f948
Jedy Wei <jedy.wei@advantech.com.tw>
2018-05-04 04:25:35 -0400

    script to install vim8

---
+++ install-vim8.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# script for CentOS only
+sudo curl -L https://copr.fedorainfracloud.org/coprs/mcepl/vim8/repo/epel-7/mcepl-vim8-epel-7.repo -o /etc/yum.repos.d/mcepl-vim8-epel-7.repo
+sudo yum update vim*
+
