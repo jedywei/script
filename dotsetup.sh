b046126327530969b74d93c4741e3f4b43e5f336
Jedy Wei <jedy.wei@advantech.com.tw>
2018-11-21 13:44:52 +0800

    remove check .bashrc

--- dotsetup.sh
+++ dotsetup.sh
@@ -1,12 +1,8 @@
 ls -al .bashrc
 
-if [ -f .bashrc ]; then
-    echo "Please remove the .bashrc before setting up all the dot file"
-    exit 1
-fi
-
 ln -sf script/.bashrc .bashrc
 ln -sf script/.gitignore .gitignore
 ln -sf script/.gdbinit .gdbinit
 ln -sf script/.gitconfig .gitconfig
 
+ls -al .bashrc
