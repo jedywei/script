1e0cdf3c9d0e948fda30038b1cc5228cdcf2e0c7
Jedy Wei <jedy.wei@advantech.com.tw>
2021-09-07 18:27:03 +0800

    terminal escapes should be surrounded by \[ and \] in order for bask to be able to correctly count printing characters

--- prompt.sh
+++ prompt.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-export PS1="[$VIMPROMPT\033[38;5;118m\u@\h\033[00m \W:${PROJ}]$\033]0;@\h:\W\007 "
+# terminal escapes should be surrounded by \[ and \] in order for bask to be able to correctly count printing characters to the 
+# beginning of line. 
+export PS1="[$VIMPROMPT\[\e[38;5;118m\]\u@\h\[\e[00m\] \W:${PROJ}]$ \[\e]0;@\h:\W\007\]"
