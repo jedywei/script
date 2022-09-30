879279a491ac41b1b5d1be4dcbbb8e234d0e40d1
Jedy Wei <jedy.wei@advantech.com.tw>
2022-09-30 18:00:18 +0800

    colordef.sh and colordef memo

---
+++ mycolor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source $(dirname $(realpath ${BASH_SOURCE[0]}))/colordef.sh
+echo -e   ${ehBlack}" Embedded Lookup Black!! "${ecX}"  - ehBlack"
+echo -e     ${ehRed}" Embedded Lookup Red!! " ${ecX}"   - ehRed"
+echo -e   ${ehGreen}" Embedded Lookup Green!! "${ecX}"  - ehGreen" 
+echo -e  ${ehYellow}" Embedded Lookup Red!! "${ecX}"    - ehYellow"
+echo -e    ${ehBlue}" Embedded Lookup Blue!! "${ecX}"   - ehBlue"
+echo -e ${ehMagenta}" Embedded Lookup Magenta!! "${ecX}"- ehMagenta"
+echo -e    ${ehCyan}" Embedded Lookup Cyan!! "${ecX}"   - ehCyan"
+echo -e   ${ehWhite}" Embedded Lookup White!! "${ecX}"  - ehWhite"
+
+
